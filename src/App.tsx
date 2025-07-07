01a09df985f48302a3f25394ff0ba94ba4abab1c
Артем Антонянц <aaaartemaa@gmail.com>
2025-07-07 21:20:02 +0500

    feat: make initial site UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import { Typography } from "antd";
+import { Input, Typography, Card, Checkbox, Flex, Radio, Button } from "antd";
+
 const { Title } = Typography;
 
 function App() {
+  const tasks = ["сделать TODO", "пофиксить баги"];
+
   return (
     <>
       <Title>todos</Title>
+      <Card>
+        <Input name="new_task" variant="underlined" placeholder="введите новую задачу..." />
+        <Flex vertical>
+          {tasks.map((task) => (
+            <Checkbox>{task}</Checkbox>
+          ))}
+        </Flex>
+        <Flex>
+          <Radio.Group defaultValue="All">
+            <Radio.Button value="All">все</Radio.Button>
+            <Radio.Button value="Active">активные</Radio.Button>
+            <Radio.Button value="Completed">выполненные</Radio.Button>
+          </Radio.Group>
+          <Button>очистить выполненные</Button>
+        </Flex>
+      </Card>
     </>
   );
 }
